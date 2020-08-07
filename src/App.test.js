1f5573ea92416494148273e2cadeec7625a73966
Jakz Aizzat <jakzaizzat@gmail.com>
2020-08-07 12:11:54 +0800

    Re-commit

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<div id="root"><App/></div>);
-  const linkElement = getByText('Temperature Converter');
-  expect(linkElement).toBeInTheDocument();
-});
